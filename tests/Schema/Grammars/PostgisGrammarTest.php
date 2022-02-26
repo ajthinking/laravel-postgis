ecf07802025b324b8f47aeb0b462377650c6c673
Anders Jurisoo <jurisoo@hotmail.com>
2022-02-26 09:18:01 +0100

    Add tests from bosnadev

--- tests/Schema/Grammars/PostgisGrammarTest.php
+++ tests/Schema/Grammars/PostgisGrammarTest.php
@@ -316,4 +316,169 @@ class PostgisGrammarTest extends BaseTestCase
     {
         return new PostgisGrammar();
     }
+
+    public function testAddingGinIndex()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->gin('foo');
+        $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+
+        $this->assertEquals(1, count($statements));
+		
+        $this->assertStringContainsString('CREATE INDEX', $statements[0]);
+        $this->assertStringContainsString('GIN("foo")', $statements[0]);
+    }
+    
+    public function testAddingGistIndex()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->gist('foo');
+        $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('CREATE INDEX', $statements[0]);
+        $this->assertStringContainsString('GIST("foo")', $statements[0]);
+    }
+
+    public function testAddingCharacter()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->character('foo', 14);
+        $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" character(14)', $statements[0]);
+    }
+
+    public function testAddingHstore()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->hstore('foo');
+        $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" hstore', $statements[0]);
+    }
+
+    public function testAddingUuid()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->uuid('foo');
+        $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" uuid', $statements[0]);
+    }
+
+    public function testAddingJsonb()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->jsonb('foo');
+        $statements = $blueprint->toSql($this->getConnection(), $this->getGrammar());
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" jsonb', $statements[0]);
+    }
+
+    public function testAddingInt4range()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->int4range('foo');
+        $statements = $blueprint->toSql(
+            $this->getConnection(),
+            $this->getGrammar()
+        );
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" int4range', $statements[0]);
+    }
+
+    public function testAddingInt8range()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->int8range('foo');
+        $statements = $blueprint->toSql(
+            $this->getConnection(),
+            $this->getGrammar()
+        );
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" int8range', $statements[0]);
+    }
+
+    public function testAddingNumrange()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->numrange('foo');
+        $statements = $blueprint->toSql(
+            $this->getConnection(),
+            $this->getGrammar()
+        );
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" numrange', $statements[0]);
+    }
+
+    public function testAddingTsrange()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->tsrange('foo');
+        $statements = $blueprint->toSql(
+            $this->getConnection(),
+            $this->getGrammar()
+        );
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" tsrange', $statements[0]);
+    }
+
+    public function testAddingTstzrange()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->tstzrange('foo');
+        $statements = $blueprint->toSql(
+            $this->getConnection(),
+            $this->getGrammar()
+        );
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" tstzrange', $statements[0]);
+    }
+
+    public function testAddingDatarange()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->daterange('foo');
+        $statements = $blueprint->toSql(
+            $this->getConnection(),
+            $this->getGrammar()
+        );
+
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" daterange', $statements[0]);
+    }
+    
+    public function testAddingTsvector()
+    {
+        $blueprint = new Blueprint('test');
+        $blueprint->tsvector('foo');
+        $statements = $blueprint->toSql(
+            $this->getConnection(),
+            $this->getGrammar()
+        );
+        
+        $this->assertEquals(1, count($statements));
+        $this->assertStringContainsString('alter table', $statements[0]);
+        $this->assertStringContainsString('add column "foo" tsvector', $statements[0]);
+    }	
 }
